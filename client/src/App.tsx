fb7c362009dbe00705ea50d9e82e6b59811b5fb8
Leo Gharib <leonargharib@gmail.com>
2020-11-24 17:53:54 -0800

    initial

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="app" style={{ height: window.innerHeight }}>
+      in development
+    </div>
+  );
+};
+
+export default App;
