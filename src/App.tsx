51920d949da5178ba36a142acb5f721a3de639ed
Leo Gharib <leonargharib@gmail.com>
2023-08-11 14:20:37 -0700

    task bar and start menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,56 @@
-import { Button } from "components/Button";
 import "./App.css";
-import HelloWorld from "components/HelloWorld";
+import backgroundImg from "assets/xp_background.jpg";
+import windowsXpIconImg from "assets/windows_xp_icon.png";
+import clsx from "clsx";
 
 function App() {
   return (
-    <div className="dark bg-background min-h-screen w-full flex justify-center items-center gap-4 flex-col">
-      <HelloWorld />
-      <Button>Click me!</Button>
+    <div
+      className={clsx(
+        "min-h-screen w-full flex items-center gap-4 flex-col justify-between",
+      )}
+      style={{
+        backgroundImage: `url(${backgroundImg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Desktop />
+      <Taskbar />
     </div>
   );
 }
 
 export default App;
+
+const Desktop = () => {
+  return <div className="flex-1" />;
+};
+
+const Taskbar = () => {
+  return (
+    <div className="w-full bg-[#3e74f0] h-[40px]">
+      {" "}
+      <StartButton />{" "}
+    </div>
+  );
+};
+
+const StartButton = () => {
+  return (
+    <button
+      className={clsx(
+        "h-full bg-[#54ae4c] text-white flex",
+        "items-center gap-2 p-4 rounded-r-xl rounded-l-md",
+        "hover:bg-[#54ae4c]/90",
+      )}
+    >
+      <img
+        src={windowsXpIconImg}
+        alt="Windows XP Icon"
+        className="w-auto h-6"
+      />
+      <span className="font-['Franklin Gothic'] font-bold italic">start</span>
+    </button>
+  );
+};
