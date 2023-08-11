fd7ed7c755bbc1492f3cc32cf9f61222a4b9c5f4
Leo Gharib <leonargharib@gmail.com>
2023-08-11 14:38:21 -0700

    update readme, logo

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const Desktop = () => {
 
 const Taskbar = () => {
   return (
-    <div className="w-full bg-[#3e74f0] h-[40px]">
+    <div className="w-full bg-[#3e74f0] h-[34px]">
       {" "}
       <StartButton />{" "}
     </div>
@@ -40,15 +40,15 @@ const StartButton = () => {
   return (
     <button
       className={clsx(
-        "h-full bg-[#54ae4c] text-white flex",
-        "items-center gap-2 p-4 rounded-r-xl rounded-l-md",
+        "h-full bg-[#54ae4c] text-white flex justify-between",
+        "items-center gap-1 pl-2 pr-6 rounded-r-xl rounded-l-md",
         "hover:bg-[#54ae4c]/90",
       )}
     >
       <img
         src={windowsXpIconImg}
         alt="Windows XP Icon"
-        className="w-auto h-6"
+        className="w-auto h-[24px]"
       />
       <span className="font-['Franklin Gothic'] font-bold italic">start</span>
     </button>
